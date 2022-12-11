d99287bef4ef9c5308067d2dcec7a01bd6981e52
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-12-11 17:51:42 +0600

    all done

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ const About = () => {
   return (
     <section className='section bg-secondary' id='about'>
       <div className='container mx-auto'>
+        <h2 className='text-center text-4xl mb-10'>About Me</h2>
         <div className='flex flex-col xl:flex-row gap-24'>
           <img
             className='object-cover h-full w-[566px] md:mx-auto lg:mx-0 rounded-2xl'
@@ -26,8 +27,8 @@ const About = () => {
                 Professional and dedicated application developer who is highly experienced in managing effective teams. As a
                 passionate developer, I am able to adapt to any kind of pressure and accept challenges. I would like to expand
                 my skills by working for a prestigious company.
-
               </p>
+              <p className='mb-8'> I love to use my creativity and make something new. That's why I love to work with React.js. Over the last year, I gather knowledge in various parts of web development. My ultimate goal is to become an awesome full-stack web developer who can make beautiful UIs and also can handle the back-end smoothly.</p>
             </div>
             <button className='btn btn-md bg-accent hover:bg-secondary-hover transition-all'>
               Contact me
